bb3f7a5e777b004c5df723343b2ba714c861e0e1
plp_Ac_2024 <jeanbaron011@gmail.com>
2024-04-04 20:34:02 +0300

    Add

---
+++ assignment.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a number:");
+  String? input = stdin.readLineSync();
+
+  if (input != null) {
+    int number = int.parse(input);
+    if (number > 10) {
+      print("Your number is greater than 10");
+    } else if (number < 10) {
+      print("Your number is less than 10");
+    } else {
+      print("Your number is equal to 10");
+    }
+  }
+}
